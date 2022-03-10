35d58d3f6d24309e5992ff1360640c95d930b5c8
Ubuntu <azureuser@projectmachine.v5drgg4ahyhejanwoe15ncishe.zx.internal.cloudapp.net>
2022-03-10 17:52:01 +0000

    adding jenkinsfolder

---
+++ jenkins/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#get environment ready
+sudo apt update
+sudo apt install python3-venv python3 pip python3 -y
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
