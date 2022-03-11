0be0c17c6f70e2fb2c09ffbc5e611165f4aacb16
Ubuntu <azureuser@projectmachine.v5drgg4ahyhejanwoe15ncishe.zx.internal.cloudapp.net>
2022-03-11 19:06:46 +0000

    updating application

--- application/__init__.py
+++ application/__init__.py
@@ -6,7 +6,7 @@ from os import getenv
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SECRET_KEY'] = ['SECRET_KEY']
+app.config['SECRET_KEY'] = 'SECRETS'
 db = SQLAlchemy(app)
 
 from application import routes
