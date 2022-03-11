2643b0864ac64f8c047d8220b59ebc983f3055bc
Ubuntu <azureuser@projectmachine.v5drgg4ahyhejanwoe15ncishe.zx.internal.cloudapp.net>
2022-03-11 10:57:15 +0000

    correcting file

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -3,6 +3,6 @@
 source venv/bin/activate
 python3 -m pytest \
 --cov=application \
---cov-report term-misssing \
+--cov-report term-missing \
 --cov-report xml:coverage.xml \
 --junitxml=junit_report.xml
